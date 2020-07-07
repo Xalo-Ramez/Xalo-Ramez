e7e64ebab60d5a254275d9b2de5089a468c6135c
Xalo-Ramez <67283751+Xalo-Ramez@users.noreply.github.com>
2020-07-08 01:44:26 +0300

    Update Cofin.js

--- Cofin.js
+++ Cofin.js
@@ -1,1 +1,9 @@
-# Xalo-Ramez
+{
+  module.exports = {
+    TOKEN: "Njg1Mzk4ODMwNTQ3ODYxNTIx.XtJnjg.BAI_BKIxE_8DnMVSsnW-IHSsnX0",
+    YT_API_KEY: "api ",
+    prefix: "$",
+    devs: ["620324892398714910"]
+  };
+}
+
